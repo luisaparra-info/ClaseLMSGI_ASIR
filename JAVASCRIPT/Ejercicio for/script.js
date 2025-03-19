d66378546924ff4f2b80428a7c8b0e4503bf2d8a
luisaparra-info <luisaparra.info@gmail.com>
2025-03-19 11:02:34 +0100

    datos

---
+++ JAVASCRIPT/Ejercicio for/script.js
@@ -0,0 +1,7 @@
+const temperaturas = [
+    [23,25,22,16,24,22,27],
+    [33,35,32,36,34,32,37],
+    [13,15,12,26,14,12,17],
+    [25,25,25,25,25,25,25]]; 
+
+const localidades = ["Vera","Huércal-Overa","Albox","Mojácar"];
